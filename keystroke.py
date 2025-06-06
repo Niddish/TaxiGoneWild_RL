23d91b70538a293ddd4c437ddf32b917acf8d75a
nwarre <121927267+Niddish@users.noreply.github.com>
2025-06-06 10:45:18 -0400

    Update keystroke.py

--- keystroke.py
+++ keystroke.py
@@ -11,3 +11,15 @@ def hold_key(key):
     time.sleep(20)
     pyautogui.keyUp(key)
     print("%s key released" % key)
+
+def reset_game():
+    coords = [
+        (920, 605),  #play again
+        (610, 807),  #start
+        (962, 892),  #confirm start
+    ]
+    for x, y in coords:
+        time.sleep(.75) #delay to prevent accidentally not starting
+        pyautogui.moveTo(x, y)
+        pyautogui.click()
+        time.sleep(.75) #delay to prevent accidentally not starting
